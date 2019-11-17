de42ac610c013ee5916d73b08f55c36436dccc72
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2019-11-17 16:20:11 +0300

    Init application

---
+++ src/main/java/com/arnatovich/goodsportal/GoodsPortalApp.java
@@ -0,0 +1,13 @@
+package com.arnatovich.goodsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsPortalApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoodsPortalApp.class, args);
+  }
+
+}
