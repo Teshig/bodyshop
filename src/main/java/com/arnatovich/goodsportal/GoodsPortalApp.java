5aa568208daec5f39708fecb4e1e63659b31fd37
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2019-11-17 16:34:28 +0300

    Explanation of basic annotations

--- src/main/java/com/arnatovich/goodsportal/GoodsPortalApp.java
+++ src/main/java/com/arnatovich/goodsportal/GoodsPortalApp.java
@@ -3,6 +3,10 @@ package com.arnatovich.goodsportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for application.
+ * SpringBootApplication = SpringBootConfiguration + EnableAutoConfiguration + ComponentScan
+ */
 @SpringBootApplication
 public class GoodsPortalApp {
 
