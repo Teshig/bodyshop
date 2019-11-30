fe715d9b3abd5d7032987b3153d6db9a5d839cbb
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2019-11-30 17:38:06 +0300

    Changed to LDAP-backed user store with embedded server

--- src/main/java/com/arnatovich/goodsportal/security/SecurityConfig.java
+++ src/main/java/com/arnatovich/goodsportal/security/SecurityConfig.java
@@ -1,16 +1,15 @@
 package com.arnatovich.goodsportal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import javax.sql.DataSource;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   
@@ -18,45 +17,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   DataSource dataSource;
   
   /**
-   * By default would be enough just auth.jdbcAuthentication().dataSource(dataSource);
-   * But in that case Spring Security will be expecting certain tables in your DB, and 
-   * will perform next queries:
+   * LDAP - Lightweight Directory Access Protocol.
+   * userSearchFilter() and groupSearchFilter() methods are used to provide filters for the base
+   * LDAP queries.
    * 
-   * public static final String DEF_USERS_BY_USERNAME_QUERY =
-   *  "select username,password,enabled " +
-   *  "from users " +
-   *  "where username = ?";
+   * Default strategy for authenticating against LDAP is to perform a bind operation, authenticating 
+   * the user directly to the LDAP server, and then the actual password will remains secured. 
    * 
-   * public static final String DEF_AUTHORITIES_BY_USERNAME_QUERY =
-   *  "select username,authority " +
-   *  "from authorities " +
-   *  "where username = ?";
+   * If for some reason you'd rather authenticate by doing a password comparison, you can declare so
+   * with the passwordCompare().
+   * By default, the password given in the login form will be compared with the value of the
+   * userPassword attribute in the user's LDAP entry. If the password is kept in a different attribute,
+   * you can specify the password attribute's name with passwordAttribute().
    * 
-   * public static final String DEF_GROUP_AUTHORITIES_BY_USERNAME_QUERY =
-   *  "select g.id, g.group_name, ga.authority " +
-   *  "from groups g, group_members gm, group_authorities ga " +
-   *  "where gm.username = ? " +
-   *  "and g.id = ga.group_id " +
-   *  "and g.id = gm.group_id";
-   *  
-   *  ####################################################################################
-   *  
-   *  You could customize your requests, as shown below, but you should adhere to the basic 
-   *  contract of the queries:
-   *  - all queries take the username as their only parameter;
-   *  - the authentication query selects the username, password and enabled status;
-   *  - the authorities query selects zero or more rows containing the username and a granted authority;
-   *  - the group authorities query selects zero or more rows, each with a group ID, a group name and an authority;
+   * By default, Spring Security's LDAP authentication assumes that the LDAP server is listening on 
+   * port 33389 on localhost. But if your LDAP server is on another machine, you can use
+   * the contextSource().url("ldap://goodcloud.com:389/dc=goodcloud,dc=com").
+   * 
+   * If you would like to use embedded LDAP server, you can specify the root suffix for the embedded
+   * server via .contextSource().root("dc=goodcloud,dc=com").
+   * 
+   * When the LDAP server starts, it will attempt to load data from any LDIF(LDAP Data Interchange Format)
+   * files that it can find in the classpath. If you would rather explicit point which LDIF files should be
+   * loaded, you can use .ldif() method.
    */
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
-        .jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery(
-            "SELECT username, password, enabled FROM Users WHERE username=?")
-        .authoritiesByUsernameQuery(
-            "SELECT username, authority FROM UserAuthorities WHERE username=?")
-        .passwordEncoder(new BCryptPasswordEncoder(4));
+        .ldapAuthentication()
+          .userSearchBase("ou=people")
+          .userSearchFilter("(uid={0})")
+          .groupSearchBase("ou=groups")
+          .groupSearchFilter("member={0}")
+          .contextSource().root("dc=goodcloud,dc=com")
+          .ldif("classpath:users.ldif")
+          .and()
+          .passwordCompare()
+          .passwordEncoder(new BCryptPasswordEncoder())
+          .passwordAttribute("passcode");
   }
 }
