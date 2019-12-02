015c6897c6794136b563381a4def98d5793d5f43
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2019-12-02 23:13:55 +0300

    Customizing user authentication

--- src/main/java/com/arnatovich/goodsportal/security/SecurityConfig.java
+++ src/main/java/com/arnatovich/goodsportal/security/SecurityConfig.java
@@ -1,59 +1,38 @@
 package com.arnatovich.goodsportal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  
+
+  @Qualifier("userRepositoryUserDetailsService")
   @Autowired
-  DataSource dataSource;
+  private UserDetailsService userDetailsService;
   
   /**
-   * LDAP - Lightweight Directory Access Protocol.
-   * userSearchFilter() and groupSearchFilter() methods are used to provide filters for the base
-   * LDAP queries.
-   * 
-   * Default strategy for authenticating against LDAP is to perform a bind operation, authenticating 
-   * the user directly to the LDAP server, and then the actual password will remains secured. 
-   * 
-   * If for some reason you'd rather authenticate by doing a password comparison, you can declare so
-   * with the passwordCompare().
-   * By default, the password given in the login form will be compared with the value of the
-   * userPassword attribute in the user's LDAP entry. If the password is kept in a different attribute,
-   * you can specify the password attribute's name with passwordAttribute().
-   * 
-   * By default, Spring Security's LDAP authentication assumes that the LDAP server is listening on 
-   * port 33389 on localhost. But if your LDAP server is on another machine, you can use
-   * the contextSource().url("ldap://goodcloud.com:389/dc=goodcloud,dc=com").
-   * 
-   * If you would like to use embedded LDAP server, you can specify the root suffix for the embedded
-   * server via .contextSource().root("dc=goodcloud,dc=com").
-   * 
-   * When the LDAP server starts, it will attempt to load data from any LDIF(LDAP Data Interchange Format)
-   * files that it can find in the classpath. If you would rather explicit point which LDIF files should be
-   * loaded, you can use .ldif() method.
+   * As it is annotated with @Bean any calls to encoder() will be intercepted to return the bean
+   * instance from the application context, not the original object.
    */
+  @Bean
+  public PasswordEncoder encoder() {
+    return new SCryptPasswordEncoder();
+  }
+  
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
-        .ldapAuthentication()
-          .userSearchBase("ou=people")
-          .userSearchFilter("(uid={0})")
-          .groupSearchBase("ou=groups")
-          .groupSearchFilter("member={0}")
-          .contextSource().root("dc=goodcloud,dc=com")
-          .ldif("classpath:users.ldif")
-          .and()
-          .passwordCompare()
-          .passwordEncoder(new BCryptPasswordEncoder())
-          .passwordAttribute("passcode");
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
   }
 }
