896b5c3ef97836f46383f375370fa4163ba51c30
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2020-02-01 22:26:43 +0300

    Adding meta data for configuration properties.

--- src/main/java/com/arnatovich/goodsportal/web/OrderProps.java
+++ src/main/java/com/arnatovich/goodsportal/web/OrderProps.java
@@ -3,6 +3,7 @@ package com.arnatovich.goodsportal.web;
 import lombok.Data;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -13,6 +14,7 @@ import javax.validation.constraints.Min;
 @ConfigurationProperties(prefix = "bodyshop.orders")
 @Data
 @Validated
+@EnableConfigurationProperties
 public class OrderProps {
   
   /**
