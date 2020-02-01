eb22535036acd98fda98aaf545762e3faf3b8635
HGRUPP\vyacheslav <Vyachselav.Arnatovich@helmes.com>
2020-02-01 22:03:57 +0300

    Adding validation to a property.

--- src/main/java/com/arnatovich/goodsportal/web/OrderProps.java
+++ src/main/java/com/arnatovich/goodsportal/web/OrderProps.java
@@ -4,12 +4,22 @@ import lombok.Data;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @Component
 @ConfigurationProperties(prefix = "bodyshop.orders")
 @Data
+@Validated
 public class OrderProps {
   
+  /**
+   * As configuration was extracted to a separate class, it's possible to add validation for this property just in one place.
+   */
+  @Min(value = 5, message = "should be greater then 5")
+  @Max(value = 25, message = "should be lower that 25")
   private int pageSize = 20;
 
 }
